8c97e7e3808661443bcf6679069e1d8596ab95b8
Denis_L <denis88lyapin@gmail.com>
2023-05-15 22:50:04 +0300

    created tests for class Item in test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price(item):
+    # Проверка, что возвращает правильную общую стоимость товара
+    expected_total_price = item.price * item.quantity
+    total_price = item.calculate_total_price()
+    assert total_price == expected_total_price
+
+
+def test_apply_discount(item):
+    # Проверка, что метод применяет скидку к цене товара
+    expected_discounted_price = item.price * Item.pay_rate
+    item.apply_discount()
+    assert item.price == expected_discounted_price
+
+
+def test_all_items_list(item):
+    # Проверка, что экземпляр класса Item добавлен в список всех товаров
+    assert len(Item.all) == 1
+    assert Item.all[0] == item
