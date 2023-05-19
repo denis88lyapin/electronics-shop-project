d60f77c8ac3a5583aee3a5e0701bcd5c09fea44a
Denis_L <denis88lyapin@gmail.com>
2023-05-19 19:07:47 +0300

    created new features

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+import math
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +18,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self._name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -37,3 +42,43 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        """
+        Геттер для атрибута _name
+        """
+        return self._name
+
+    @name.setter
+    def name(self, new_name) -> None:
+        """
+        Сеттер для атрибута _name. Проверяет количество символов.
+        Доустимо не более 10-ти.
+        """
+        if len(new_name) <= 10:
+            self._name = new_name
+        else:
+            raise ValueError("Длина наименования товара превышает 10 символов")
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """
+        класс-метод, инициализирующий экземпляры класса `Item`
+        данными из файла _src/items.csv_
+        """
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        csv_file = os.path.join(current_dir, 'items.csv')
+        Item.all = []
+        with open(csv_file, encoding="cp1251") as csvfile:
+            item_reader = csv.DictReader(csvfile)
+            for i in item_reader:
+                cls(name=i["name"], price=float(i["price"]), quantity=int(i["quantity"]))
+
+    @staticmethod
+    def string_to_number(string) -> int:
+        try:
+            num = int(math.floor(float(string)))
+            return num
+        except ValueError:
+            raise ValueError("Ошибка: Невозможно преобразовать строку в число.")
