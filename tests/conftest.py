199a8b5a82fcfda801abe03a2c10077cc41bb171
Denis_L <denis88lyapin@gmail.com>
2023-05-29 21:43:13 +0300

    created fixture Phone in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,17 @@
 import pytest
 from src.item import Item
+from src.phone import Phone
+
 
 @pytest.fixture
 def item():
     Item.all = []
     item = Item(name="Товар", price=10.0, quantity=5)
     return item
+
+
+@pytest.fixture
+def phone():
+    Phone.all = []
+    phone = Phone(name='iPhone 14', price=120000, quantity=5, number_of_sim=1)
+    return phone
