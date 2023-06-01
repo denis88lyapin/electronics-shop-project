54617896559c2e5da53a885e2625e375198b7db6
Denis_L <denis88lyapin@gmail.com>
2023-06-01 23:41:24 +0300

    created class KeyBoard to keyboard.py

---
+++ src/keyboard.py
@@ -0,0 +1,59 @@
+from src.item import Item
+
+
+class LanguageMixin:
+    """
+    Mixin для хранения и изменения раскладки клавиатуры (EN, RU).
+    По умолчанию - EN.
+    """
+    def __init__(self) -> None:
+        self._language = "EN"
+
+    def change_lang(self) -> object:
+        """
+        Функция изменения раскладки клавиатуры.
+        """
+        if self._language == "EN":
+            self._language = "RU"
+        else:
+            self._language = "EN"
+        return self
+
+    @property
+    def language(self) -> str:
+        """
+        Защита от возможности установить другую раскладку.
+        """
+        return self._language
+
+    @language.setter
+    def language(self, new_language: str) -> Exception:
+        raise AttributeError("property 'language' of 'KeyBoard' object has no setter")
+
+
+class KeyBoard(Item, LanguageMixin):
+    """
+    Класс для товара “клавиатура”
+    """
+
+    def __init__(self, name: str, price: float, quantity: int) -> None:
+        super().__init__(name, price, quantity)
+        LanguageMixin.__init__(self)
+
+    @property
+    def name(self):
+        """
+        Переопределение геттера для атрибута _name
+        """
+        return self._name
+
+    @name.setter
+    def name(self, new_name) -> None:
+        """
+        Переопределение сеттера для атрибута _name. Проверяет количество символов.
+        Допустимо не более 30-ти.
+        """
+        if len(new_name) <= 30:
+            self._name = new_name
+        else:
+            raise ValueError("Длина наименования товара превышает 30 символов")
