e39f0d09a8dec91dd8a0c3eb5684c784cc82d43a
Denis_L <denis88lyapin@gmail.com>
2023-06-07 20:14:24 +0300

    added exception for instantiate_from_csv method in instantiate_CSV_error.py

--- src/instantiate_CSV_error.py
+++ src/instantiate_CSV_error.py
@@ -1,3 +1,9 @@
 class InstantiateCSVError(Exception):
-    def __init__(self, *args, **kwargs):
-        if args
+    """
+    Класс - исключение, если item.csv поврежден (например, отсутствует одна из колонок данных)
+    """
+    def __init__(self):
+        self.massage = "Файл item.csv поврежден"
+
+    def __str__(self):
+        return self.massage
