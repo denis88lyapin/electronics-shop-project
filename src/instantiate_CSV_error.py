175105860b9801205a45a4a7cbc0d273e1570b2e
Denis_L <denis88lyapin@gmail.com>
2023-06-07 21:47:05 +0300

    fixed instantiate_CSV_error.py

--- src/instantiate_CSV_error.py
+++ src/instantiate_CSV_error.py
@@ -3,7 +3,7 @@ class InstantiateCSVError(Exception):
     Класс - исключение, если item.csv поврежден (например, отсутствует одна из колонок данных)
     """
     def __init__(self):
-        self.massage = "Файл item.csv поврежден"
+        self.massage = "Поврежден файл"
 
     def __str__(self):
         return self.massage
